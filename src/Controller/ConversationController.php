db4d19bb029217d1c1ead8883af09f9617bd5d9d
Emilie <emilie.chnvt@gmail.com>
2025-05-26 15:43:15 +0200

    user profile message conversation entity added

---
+++ src/Controller/ConversationController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProfileRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ConversationController extends AbstractController
+{
+    #[Route('/', name: 'app_conversations')]
+    public function index(ProfileRepository $profileRepository): Response
+    {
+        if(!$this->getUser()){
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('conversation/index.html.twig', [
+            'profiles' => $profileRepository->findAll(),
+        ]);
+    }
+}
